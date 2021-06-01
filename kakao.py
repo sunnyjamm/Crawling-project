3a1807f3df0a9aa845cf2489dbfee9a66f3a3a25
FLY-CODE77 <lightyear94122@gmail.com>
2021-06-01 19:47:26 +0900

    kakao api module and add Docstring

---
+++ kakao.py
@@ -0,0 +1,71 @@
+def kakao( REST_API_KEY, target, obj):
+
+    '''
+    kakao(
+    REST_API_KEY,
+    target, 
+    obj
+    ), 
+
+    Docstring:
+    USE KAKAO API CRAWLING DATA
+    retrun obj_dataframe 
+    
+    REST_API_KEY: kakao api key, 
+    target : zigbang df 
+    obj : category of data
+          { "oneroom", "apartment", "villa"}
+    
+    Examples
+    --------
+    kakao(REST_API_KEY, zigbang_villa_df, 2)
+    return kakao_villa_df
+    ''' 
+
+    headers = {"Authorization": f"KakaoAK {REST_API_KEY}",}
+
+    cate_ls =  ["MT1", "CS2", "PS3", "SC4", "AC5", "PK6", "OL7", "SW8", "BK9", "CT1", "AG2", "PO3", "AT4",
+                "AD5", "FD6", "CE7", "HP8", "PM9"]
+
+    page, size, radius = 1, 15, 200
+    dfs_2 = []
+    obj[cate_ls] = None
+    for idx, loc in enumerate(obj[['lat', 'lng']].values):
+        print(idx, end=" ")
+        lat, lng = loc
+        for category in cate_ls:
+            while True:
+                url = f"https://dapi.kakao.com/v2/local/search/category.json?category_group_code={category}&page={page}&size={size}&sort=distance&x={lng}&y={lat}&radius={radius}"
+                response = requests.get(url, headers=headers)
+
+                datas = response.json()['documents']
+
+                df = pd.DataFrame(datas)
+                df['매물_lat'] = lat
+                df['매물_lng'] = lng
+
+                dfs_2.append(df)
+
+                if response.json()['meta']['is_end']:
+                    obj.loc[idx, category] = response.json()['meta']['total_count']
+                    break
+
+                page += 1
+
+    if target == "oneroom":
+        kakao_oneroom_df = pd.concat(dfs_2).reset_index(drop=True)
+        return kakao_oneroom_df
+    
+    elif target == "apartment":
+        kakao_apt_df = pd.concat(dfs_2).reset_index(drop=True)
+        return kakao_apt_df
+    
+    elif target == "villa":
+        kakao_villa_df = pd.concat(dfs_2).reset_index(drop=True)
+        return kakao_villa_df
+    
+    else:
+        kakao_office_df = pd.concat(dfs_2).reset_index(drop=True)
+        return kakao_office_df
+
+    print("end kakao crawling")
